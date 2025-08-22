aafc780f4d3e997ad1bdbe70517b67e530878f5a
Jorge Henriquez <jhenriquez@supertrans.cl>
2025-08-21 23:56:59 -0400

    Version probando Webhook

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+
 Route::post('/webhook/dispatchtrack', [WebhookController::class, 'dispatchtrack']);
 
 require __DIR__.'/auth.php';
